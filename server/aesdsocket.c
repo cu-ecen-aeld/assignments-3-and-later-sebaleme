42ae072ec69ef9680281c59627d62370cca39432
Lemetter Sebastien (XC-AS/EAF3-ADA) <Sebastien.Lemetter@de.bosch.com>
2024-10-06 13:21:22 +0200

    Feature: prepare multithreading, move helper functions to header

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -2,86 +2,21 @@
  * aesdsocket.c: Create a socket connection
  * ========================================== */
 #include <errno.h>
-#include <fcntl.h>
 #include <netdb.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <syslog.h>
-#include <unistd.h>
 
-#define PORT "9000"
+
+#include "aesdsocket.h"
+
 #define BACKLOG 10
 #define FILEPATH "/var/tmp/aesdsocketdata"
 #define BUFFER_SIZE 2000
 
-// (IPv4 only--see struct sockaddr_in6 for IPv6)
-// struct sockaddr_in {
-//     short int          sin_family;  // Address family, AF_INET
-//     unsigned short int sin_port;    // Port number
-//     struct in_addr     sin_addr;    // Internet address
-//     unsigned char      sin_zero[8]; // Same size as struct sockaddr
-// };
-
-/// Create struct for thread information
-struct CThreadInstance
-{
-    int fd ; // accepted socket connection identifyer, unique for each thread
-    struct sockaddr_storage client_addr; // Describes the socket address.
-    socklen_t addr_size; // Address size, depends of address type (IPv4 or IPv6)
-};
-
-
-/// Function which transfer process to a deamon
-/// A deamon is running outside any console, in the system background
-void create_deamon()
-{        
-    // creating child process which is not attached to a TTY
-    int pid = fork();
-    // An error occurred
-    if (pid < 0)
-        exit(EXIT_FAILURE);
-    // Success: Let the parent terminate so grand parent can proceed
-    if (pid > 0)
-        exit(EXIT_SUCCESS);
-
-    // Create a new session so daemon is not linked to any tty
-    if (setsid() < 0)
-        exit(EXIT_FAILURE);
-
-    // Change the working directory to the root directory so no possible error if unmount
-    chdir("/");
-
-    // Redirect stdin, stdout and stderr to /dev/null, so our daemon won t communicate through a console
-    close(0); close(1); close(2);
-    open("/dev/null",O_RDWR); dup(0); dup(0);
-}
-
-/// Function initializing the socket, prepares the future connections
-int createSocketConnection(struct addrinfo* my_addr)
-{
-    // Create socket and bind it to given port
-    int socket_fd = socket(PF_INET, SOCK_STREAM, 0);
-    // first, load up address structs with getaddrinfo():
-    struct addrinfo hints;
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET; // use IPv4 or IPv6, whichever
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // fill in my IP for me
-    int status = getaddrinfo(NULL, PORT, &hints, &my_addr);
-    if (status != 0 || my_addr == NULL)
-    {
-        return -1;
-    }
-
-    // Assign an address to the socket
-    bind(socket_fd, my_addr->ai_addr, sizeof(struct sockaddr));
-    syslog(LOG_INFO, "Socket created and binded, with file descriptor %d\n", socket_fd);
-    return socket_fd;
-}
 
 int main(int argc, char** argv)
 {
